ed8d9edba69eabb85544c2cb266ff540d178beaf
Yoann Emmanuel <emmanuelyoann19@gmail.com>
2026-05-28 22:25:29 +0000

    fix: set storage permissions during Docker build

--- api/start.sh
+++ api/start.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+# Permissions storage
+chmod -R 775 /var/www/html/storage
+chmod -R 775 /var/www/html/bootstrap/cache
+
+# Migrations
+php /var/www/html/artisan migrate --force
+
+# Démarrage
 php-fpm -D
 service nginx start
 tail -f /var/log/nginx/error.log
