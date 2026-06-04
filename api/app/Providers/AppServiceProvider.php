08472299008dfffd3daffefc73db7942586b4022
Yoann Emmanuel <emmanuelyoann19@gmail.com>
2026-06-04 00:24:10 +0000

    chore: docker config for render deployment

--- api/app/Providers/AppServiceProvider.php
+++ api/app/Providers/AppServiceProvider.php
@@ -2,6 +2,11 @@
 
 namespace App\Providers;
 
+use Cloudinary\Cloudinary;
+use App\Flysystem\CloudinaryAdapter;
+use Illuminate\Filesystem\FilesystemAdapter;
+use Illuminate\Support\Facades\Storage;
+use League\Flysystem\Filesystem;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -17,5 +22,25 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void {}
+    public function boot(): void
+{
+    Storage::extend('cloudinary', function ($app, $config) {
+        $cloudinary = new Cloudinary([
+            'cloud' => [
+                'cloud_name' => $config['cloud_name'],
+                'api_key'    => $config['api_key'],
+                'api_secret' => $config['api_secret'],
+            ],
+        ]);
+
+        $adapter    = new CloudinaryAdapter($cloudinary);
+        $filesystem = new Filesystem($adapter);
+
+        return new FilesystemAdapter(
+            $filesystem,
+            $adapter,
+            $config,
+        );
+    });
+}
 }
